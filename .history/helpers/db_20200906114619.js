77e86e6328979e0db261968af699270116d964c6
Alper <uslualper@outlook.com>
2020-09-12 12:46:28 +0300

    Remove duplicated directory

--- .history/helpers/db_20200906114619.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-//mongodb://localhost/
-module.export = () =>{
-    mongoose.connect('mongodb://localhost/');
-}
--- .history/helpers/db_20200906114709.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-//mongodb://localhost/
-module.export = () =>{
-    mongoose.connect('mongodb://localhost/');
-    mongoose.connect.on('open',()=>{
-        console.log('Mongodb connected');
-    });
-}
